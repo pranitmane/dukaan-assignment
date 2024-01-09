0f235ca509cb30b17aa2cb151bbdbcd8cdad31d7
pranit mane <pranitmane@pranits-MacBook-Pro.local>
2024-01-09 17:19:02 +0530

    minor fix

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
             Hello I'm <a className='hover:text-Blue underline' target='blank' href="https://twitter.com/pranitbmane">Pranit</a>,  
             </p>
             <p>
-            As a third year Computer Engineering student I am looking for Internship oppurtunities, and what better place to work than the hardworking team at <a className='underline hover:text-Blue' target='blank' href="https://mydukaan.io">Dukaan</a>, It would be a great learning experience for me to work on with some of the challenges.
+            As a third year Computer Engineering student I am looking for Internship role, and what better place to work than with the hardworking team at <a className='underline hover:text-Blue' target='blank' href="https://mydukaan.io">Dukaan</a>, It would be a great learning experience for me to work on with real life challenges.
             </p>
             </div>
             <div className='flex flex-row gap-2'>
