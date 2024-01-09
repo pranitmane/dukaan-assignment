00578f1a60836a99e22ffaa4ee03586b88cc03e8
pranit mane <pranitmane@pranits-MacBook-Pro.local>
2024-01-09 20:38:11 +0530

    design fix

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,12 +2,12 @@ import {Link} from 'react-router-dom';
 export default function HomePage() {
     return (
         <div className="flex flex-col items-center justify-center w-full">
-            <div className="text-black fixed bottom-4 p-4">
+            <div className="text-Black30 fixed bottom-4 p-4">
             <p>
             Hello I'm <a className='hover:text-Blue underline' target='blank' href="https://twitter.com/pranitbmane">Pranit</a>,  
             </p>
             <p>
-            As a third year Computer Engineering student I am looking for Internship role, and what better place to work than with the hardworking team at <a className='underline hover:text-Blue' target='blank' href="https://mydukaan.io">Dukaan</a>, It would be a great learning experience for me to work on with real life challenges.
+            As a third year Computer Engineering student I am looking for Internship role, and what better place to work than with the hardworking team at <a className='underline hover:text-Blue' target='blank' href="https://mydukaan.io">Dukaan</a>, It would be a great learning experience for me to work on real life challenges.
             </p>
             </div>
             <div className='flex flex-row gap-2'>
