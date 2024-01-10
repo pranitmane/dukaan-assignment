4ea3c10973028ce988af776f148f695112498f25
pranit mane <pranitmane@pranits-MacBook-Pro.local>
2024-01-10 14:51:11 +0530

    making it reponsive

--- src/components/paymetsPage/paymentStats.jsx
+++ src/components/paymetsPage/paymentStats.jsx
@@ -2,7 +2,7 @@
 
 export default function PaymentStats() {
     return (
-      <div className="flex flex-row gap-5 pb-8">
+      <div className="flex md:flex-row flex-col gap-3 md:gap-5 pb-8">
         <div className="flex-1 p-5 bg-Black100 shadow-base flex-col rounded-plus gap-4">
           <p className="text-Black30 text-lg1">Online orders</p>
           <p className="text-2xl text-Black12">231</p>
